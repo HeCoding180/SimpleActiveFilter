4bb78703de9d60b13833fe47b9f5b62bc1108cfc
HeCoding180 <HeCoding180@outlook.com>
2022-05-24 17:03:45 +0200

    Class filished

--- SimpleActiveFilter/ActiveFilterEngine.cs
+++ SimpleActiveFilter/ActiveFilterEngine.cs
@@ -23,13 +23,21 @@ namespace SimpleActiveFilter
     {
         public FilterType filterType { set; get; }
 
+        //Attributes for 
+        public bool UseFiniteOpenLoopGain { set; get; }
+        public double UnityGainFrequency { set; get; }
+        public double OpenLoopGain { set; get; }
+
+        //Part Attributes
         public double ResistorA { set; get; }   //Resistance, for the lowpass characteristic
         public double CapacitorA { set; get; }  //Capacitance, for the lowpass characteristic
         public double ResistorB { set; get; }   //Resistance, for the highpass characteristic
         public double CapacitorB { set; get; }  //Capacitance, for the highpass characteristic
 
+        //Constructors
         public ActiveFilterEngine(FilterType type)
         {
+            UseFiniteOpenLoopGain = false;
             filterType = type;
             ResistorA = 0.0f;
             ResistorB = 0.0f;
@@ -38,50 +46,101 @@ namespace SimpleActiveFilter
         }
         public ActiveFilterEngine(FilterType type, double Ra, double Rb, double Ca, double Cb)
         {
+            UseFiniteOpenLoopGain = false;
             filterType = type;
             ResistorA = Ra;
             ResistorB = Rb;
             CapacitorA = Ca;
             CapacitorB = Cb;
         }
-        public ActiveFilterEngine(double Ra, double Rb, double Ca, double Cb)
+        public ActiveFilterEngine(FilterType type, double Ra, double Rb, double Ca, double Cb, double fUnityGain, double AOL)
         {
-            filterType = FilterType.BP;
+            UseFiniteOpenLoopGain = true;
+            filterType = type;
             ResistorA = Ra;
             ResistorB = Rb;
             CapacitorA = Ca;
             CapacitorB = Cb;
+            UnityGainFrequency = fUnityGain;
+            OpenLoopGain = AOL;
         }
 
-        private ComplexImpedance getZa(double frequency)
+        //Calculation Methods (private)
+        private double GetZa(double frequency)
         {
-            ComplexImpedance result = new ComplexImpedance();
+            double result;
             switch (filterType)
             {
                 case FilterType.HP:
-                    result.Impedance = ResistorA;
-                    result.Phi = 0;
+                    result = ResistorA;
                     break;
                 default:
+                    double Ga = 1 / ResistorA;
+                    double Ba = 2 * Math.PI * frequency * CapacitorA;
+                    result = 1 / PythagoricTheorem(Ga, Ba);
                     break;
             }
             return result;
         }
         
-        private ComplexImpedance getZa(double frequency)
+        private double GetZb(double frequency)
         {
-            ComplexImpedance result = new ComplexImpedance();
+            double result;
             switch (filterType)
             {
                 case FilterType.LP:
-                    result.Impedance = ResistorB;
-                    result.Phi = 0;
+                    result = ResistorB;
                     break;
                 default:
+                    double Xcb = 1 / (2 * Math.PI * frequency * CapacitorB);
+                    result = PythagoricTheorem(ResistorB, Xcb);
                     break;
             }
-
             return result;
+        }
+
+        private double GetGainBandwidthProduct(double frequency)
+        {
+            return 1 / (1 / OpenLoopGain + frequency / UnityGainFrequency);
+        }
+
+        private double GetBareGain(double frequency)
+        {
+            return 1 + (GetZa(frequency) / GetZb(frequency));
+        }
+
+        //Calculation Methods (public)
+        public double GetGain(double freqency)
+        {
+            if (!UseFiniteOpenLoopGain)  return GetBareGain(freqency);                                                   //Return the bare gain of the Filter
+            else                         return 1 / (1 / GetBareGain(freqency) + 1 / GetGainBandwidthProduct(freqency)); //Return the gain including OpAmp characteristics
+        }
+
+        public double GetPhase(double frequency)
+        {
+            return GetLogarythmicDerivative(GetGain, frequency);
+        }
+
+        //Private Utility Methods
+        private double GetLogarythmicDerivative(Func<double, double> referenceFunction, double frequency)
+        {
+            double fLog = Math.Log10(frequency);                //Logarythmic representation of the derivative frequency
+            double fPre = Math.Pow(10, fLog - fLog / 1000);     //Logarythmically 99.9% of the derivative frequency
+            double fPost = Math.Pow(10, fLog + fLog / 1000);    //Logarythmically 100.1% of the derivative frequency
+            double fDelta = fPost - fPre;
+
+            return (referenceFunction(fPost) - referenceFunction(fPre)) / fDelta;
+        }
+
+        public static double PythagoricTheorem(double a, double b)
+        {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+        
+        //Public Utility Methods
+        public static double RadToDeg(double rad)
+        {
+            return rad / Math.PI * 180;
         }
     }
 }
--- SimpleActiveFilter/Program.cs
+++ SimpleActiveFilter/Program.cs
@@ -1,7 +1,71 @@
-﻿public class Program
+﻿namespace SimpleActiveFilter
 {
-	public static void Main()
+	public class Program
 	{
+		public static void Main()
+		{
+			//Example Code
+			ActiveFilterEngine filterEngine;
 
+			Console.Write("Enter Filter Type [LP] for LowPass [HP] for HighPass [BP] for BandPass: ");
+			string ftypeStr = Console.ReadLine().Replace("\n", "");
+			FilterType ftype = FilterType.BP;
+			switch (ftypeStr.ToUpper())
+            {
+				case "HP":
+					ftype = FilterType.HP;
+					break;
+				case "LP":
+					ftype = FilterType.LP;
+					break;
+            }
+
+			Console.WriteLine("Enter Full File Path for the CSV File: ");
+			string filePath = Console.ReadLine().Replace("\n", "");
+
+			Console.WriteLine("floating point character: '" + (1.1f).ToString().Replace("1", "") + "'");
+			Console.Write("Enter Value for Resistor A (Lowpass characteristic, unit=Ohm): ");
+			double Ra = double.Parse(Console.ReadLine().Replace("\n", ""));
+			Console.Write("Enter Value for Capacitor A (Lowpass characteristic), unit=Farad: ");
+			double Ca = double.Parse(Console.ReadLine().Replace("\n", ""));
+			Console.Write("Enter Value for Resistor B (Highpass characteristic), unit=Ohm: ");
+			double Rb = double.Parse(Console.ReadLine().Replace("\n", ""));
+			Console.Write("Enter Value for Capacitor B (Highpass characteristic), unit=Farad: ");
+			double Cb = double.Parse(Console.ReadLine().Replace("\n", ""));
+
+			Console.Write("Use Finite Gain OpAmp [1] [0]: ");
+			bool FiniteGain = ParseBool(Console.ReadLine().Replace("\n", ""));
+
+			if (FiniteGain)
+            {
+				Console.Write("Enter Value for the Gain Bandwidth Product: ");
+				double GBP = double.Parse(Console.ReadLine().Replace("\n", ""));
+				Console.Write("Enter Value for the Open Loop Gain: ");
+				double AOL = double.Parse(Console.ReadLine().Replace("\n", ""));
+
+				filterEngine = new ActiveFilterEngine(ftype, Ra, Rb, Ca, Cb, GBP, AOL);
+			}
+			else
+            {
+				filterEngine = new ActiveFilterEngine(ftype, Ra, Rb, Ca, Cb);
+			}
+
+			string csvOutputString = "Frequency;Output Gain, Phase Shift\n";
+
+			for (int fDecade = 1; fDecade <= 10000000/*10MHz*/; fDecade *= 10)
+			{
+				for (double fDecadeOffset = 1; fDecadeOffset <= 9; fDecadeOffset += 1)
+				{
+					csvOutputString += (fDecade * fDecadeOffset).ToString() + ";" + filterEngine.GetGain(fDecade * fDecadeOffset).ToString() + ";" + filterEngine.GetPhase(fDecade * fDecadeOffset).ToString() + "\n";
+				}
+			}
+
+			File.WriteAllText(filePath, csvOutputString);
+		}
+
+	    public static bool ParseBool(string inputStr)
+        {
+			return (inputStr == "1") ? true : false;
+		}
 	}
 }
